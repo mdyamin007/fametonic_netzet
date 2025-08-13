4d6557c2b887cb2225c60b4f344bfb5e11401696
mdyamin007 <mohammadyamin80@gmail.com>
2025-08-13 22:48:21 +0600

    feat: topbar added

---
+++ components/TopBar.tsx
@@ -0,0 +1,15 @@
+export default function TopBar() {
+  return (
+    <div className="py-2.5 text-center bg-gradient-to-r from-primary to-secondary">
+      <p className="font-figtree text-[22px]">
+        🚀{" "}
+        <span className="text-secondary font-extrabold">
+          FRESH BEGINNINGS SALE:{" "}
+        </span>
+        <span className="font-semibold text-white">
+          Extra 25% OFF, Limited Spots - start your journey today!
+        </span>
+      </p>
+    </div>
+  );
+}
