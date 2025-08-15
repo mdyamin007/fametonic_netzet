b9ab53fd4d972c75b8a69a716d2cb5efd71a65f6
mdyamin007 <mohammadyamin80@gmail.com>
2025-08-15 20:45:39 +0600

    feat: navbar design added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,8 +2,17 @@ import BrandLogo from "./icons/BrandLogo";
 
 export default function Navbar() {
   return (
-    <nav className="absolute top-[81px] px-[211px] flex justify-between">
+    <nav className="absolute top-[81px] w-full px-[211px] flex justify-between">
       <BrandLogo />
+
+      <ul className="flex gap-10">
+        <li className="font-figtree font-semibold text-lg text-[#A9A9A9]">
+          About us
+        </li>
+        <li className="font-figtree font-semibold text-lg text-[#A9A9A9]">
+          Contact
+        </li>
+      </ul>
     </nav>
   );
 }
